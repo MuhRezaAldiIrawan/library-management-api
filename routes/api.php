45f60d750fe80e093140b0cdd2f99e49e8a93edf
Muh Reza Aldi Irawan <titaniaelvs@gmail.com>
2025-11-20 16:42:24 +0800

    feat: borrow and return management

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\BorrowController;
 
 
 /*
@@ -36,6 +37,13 @@ Route::prefix('v1')->group(function () {
 
         // Book routes
         Route::apiResource('books', BookController::class);
+
+        Route::prefix('borrows')->group(function () {
+            Route::get('/', [BorrowController::class, 'index']);
+            Route::post('/', [BorrowController::class, 'borrow']);
+            Route::post('return', [BorrowController::class, 'return']);
+            Route::get('{borrow}', [BorrowController::class, 'show']);
+        });
 
     });
 });
